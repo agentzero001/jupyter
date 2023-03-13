6821a9fd96ba7344df0157126db182c31d3fd2db
agentzero001 <Jens.marhoefer001@gmail.com>
2023-03-13 17:00:21 +0100

    add module

--- statistics and propability theory/boxplot.py
+++ statistics and propability theory/boxplot.py
@@ -1,6 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import statistics
+import scipy.stats
+
+def boxplot(data):
+    '''Input data has to be a list of positive numbers'''
+    
+    print(scipy.stats.describe(data))
+    
+    mean = statistics.fmean(data)
+    hmean = statistics.harmonic_mean(data)
+    gmean = statistics.geometric_mean(data)
+    median = statistics.median(data)
+    
+    plt.figure(facecolor='0.6',figsize=(10,1.1),dpi=300)
+    plt.axes().set_facecolor('0.4')
+    plt.axis([min(data)-2,max(data)+2,-0.5,0.5])
+
+    plt.scatter(data,np.zeros(len(data)),color = 'black',zorder=2)
+    plt.axvline(x = gmean,color='red', linestyle='--',label='gmean')
+    plt.axvline(x = hmean,color='blue', linestyle='--',zorder=1,label='hmean')
+    plt.axvline(x = mean,color='purple', linestyle='--',label='armean')
+    plt.axvline(x = median,color='yellow', linestyle='--',label='median')
+    
+    plt.yticks(np.arange(0))
+    plt.legend(loc='lower right',prop={'size': 3})
+    plt.legend().get_frame().set_alpha(0.1)
+    plt.grid()
+    plt.show()
 
 def boxplot2(data,prop,p=0):
 
