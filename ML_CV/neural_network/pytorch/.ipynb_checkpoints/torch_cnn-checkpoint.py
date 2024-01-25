c4f241da102d9ad78182f91c5fd080f2e6f7651b
agentzero001 <Jens.marhoefer001@gmail.com>
2024-01-26 00:16:10 +0100

    -

--- ML_CV/neural_network/pytorch/.ipynb_checkpoints/torch_cnn-checkpoint.py
+++ ML_CV/neural_network/pytorch/.ipynb_checkpoints/torch_cnn-checkpoint.py
@@ -8,9 +8,14 @@ import matplotlib.pyplot as plt
 import numpy as np
  
 
-class NN(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(NN, self).__init__()
+class CNN(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv1 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+                
         
     def forward(self, x):
         pass
