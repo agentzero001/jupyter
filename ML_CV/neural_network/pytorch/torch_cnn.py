b24390fc9e9352a0f6eca3df258d92b452af9dca
agentzero001 <Jens.marhoefer001@gmail.com>
2024-01-29 05:52:18 +0100

    -

--- ML_CV/neural_network/pytorch/torch_cnn.py
+++ ML_CV/neural_network/pytorch/torch_cnn.py
@@ -11,21 +11,26 @@ import numpy as np
 class CNN(nn.Module):
     def __init__(self, in_channels=1, num_classes=10):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3,3), stride=(1,1), padding=(1,1))
         self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        self.conv1 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1))
         self.fc1 = nn.Linear(16*7*7, num_classes)
                 
         
     def forward(self, x):
-        pass
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
     
+        return x
 
-
+    
     
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-input_size = 784
 num_classes = 10
 learning_rate = .001
 batch_size = 64
@@ -39,10 +44,10 @@ test_dataset = datasets.MNIST(root=mnist_path, train=False, download=True,
                               transform=transforms.ToTensor())
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader =  DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader =  DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
 
-model = NN(input_size, num_classes).to(device)
+model = CNN().to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
